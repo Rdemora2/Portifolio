f493726ab7df8bd3d7f6cea67a388ddd3d2d3e9e
Rdemora2 <robertomoraeszar@gmail.com>
2024-05-26 00:33:36 -0300

    REFACTOR/ altera frase do about

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -24,13 +24,13 @@ const About: React.FC<Props> = ({ toggleTheme }) => {
                 </SubTitle>
 
                 <Description>
-                    Comecei minha jornada na programação em 2021, de lá pra explorei diversas linguagens/frameworks e descobri que sou um apaixonado por tecnologia e, principalmente, resolver problemas por meio da tecnologia.
+                Comecei minha jornada na programação em 2021. Desde então, explorei diversas linguagens e frameworks, e descobri que sou <strong>apaixonado por tecnologia</strong> e, principalmente, por <strong>resolver problemas por meio da tecnologia</strong>.
                 </Description>
                 <Description>
-                    Atualmente atuo com <strong>desenvolvimento web/mobile full stack</strong>, possuindo experiência abrangente em JavaScript, TypeScript, Node.js, React, React Native, Vue.js, Python, Django, Django Rest Framework, PostgreSQL, AWS e Google Cloud para criar aplicações de alto nível e valor.
+                    Atualmente atuo com <strong>desenvolvimento web/mobile full stack</strong>, possuindo experiência abrangente em JavaScript, TypeScript, Node.js, React, React Native, Vue.js, Python, Django, Django Rest Framework, PostgreSQL, AWS e Google Cloud para criar <strong>aplicações de alto nível e valor</strong>.
                 </Description>
                 <Description>
-                    Meu foco é aprender novas coisas e aprimorar ainda mais as habilidades que possuo, gosto sempre de estar em constante aprendizado e evoluindo dia após dia, e como consequência, sempre agregando valor aos projetos que participo.</Description>
+                    Meu foco é aprender novas coisas e aprimorar ainda mais as habilidades que possuo, gosto sempre de estar em constante aprendizado e evoluindo dia após dia, e como consequência, sempre <strong>agregando valor</strong> aos projetos que participo.</Description>
                 <ContainerButtons>
                     <ContactButton href="#contato">
                         <TextButton>
